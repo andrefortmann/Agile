2b3b798b79d5184faffbb192bcded0505c22c0b9
Sofia Silva <up201505628@fe.up.pt>
2019-11-14 11:47:03 +0100

    login :white_check_mark:

--- ideabay/resources/views/pages/login.blade.php
+++ ideabay/resources/views/pages/login.blade.php
@@ -1,31 +1,56 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<main>
-    <form class="form-signin">
-        <div class="text-center mb-4">
-          <img class="mb-4" src="" alt="" width="72" height="72">
-          <h1 class="h3 mb-3 font-weight-normal">Floating labels</h1>
-          <p>Build form controls with floating labels via the <code>:placeholder-shown</code> pseudo-element. <a href="https://caniuse.com/#feat=css-placeholder-shown">Works in latest Chrome, Safari, and Firefox.</a></p>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="{{{ asset('assets/favicon.ico') }}}">
+    <title>IdeaBay</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src={{ asset('js/app.js') }} defer></script>
+</head>
+
+<body>
+    <div id="login-container">
+        <div class="row">
+            <div class="col login-banner">
+                <div class="d-flex flex-column justify-content-center d-flex justify-content-center">
+                    <h1>Ideabay</h1>
+                    <p>The only platform that brings investors and entrepreneurs together. Sign up now to use all features!</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="d-flex flex-column justify-content-center d-flex justify-content-center">
+                    <form class="form-signin">
+                        <div class="text-center mb-4">
+                            <img class="mb-4" src="" alt="" width="72" height="72">
+                            <h1 class="h3 mb-3 font-weight-normal">Start sharing your ideas!</h1>
+                        </div>
+                        {{-- <div class="form-label-group">
+                            <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+                            <label for="inputUsername">Username</label>
+                        </div>  --}} 
+                        <div class="form-label-group">
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+                            <label for="inputEmail">Email</label>
+                        </div>
+                        <div class="form-label-group">
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                            <label for="inputPassword">Password</label>
+                        </div>
+                        {{-- <div class="form-label-group">
+                            <input type="password" id="inputPasswordRepeat" class="form-control" placeholder="Repeat Password" required>
+                            <label for="inputPasswordRepeat">Repeat Password</label>
+                        </div> --}}
+                        <button class="btn btn-lg btn-primary btn-block" type="submit">Log In</button>
+                        <p class="mt-5">New to Ideabay? <a href="#">Create an account.</a></p>
+                    </form>
+                </div>
+            </div>
         </div>
-      
-        <div class="form-label-group">
-          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-          <label for="inputEmail">Email address</label>
-        </div>
-      
-        <div class="form-label-group">
-          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-          <label for="inputPassword">Password</label>
-        </div>
-      
-        <div class="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2019</p>
-      </form>
-</main>
-@endsection
+    </div>
+</body>
+
+</html>
