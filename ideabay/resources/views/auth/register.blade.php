d1b732e1b1cf5f9ae3a350772766d85450c0c136
Sofia Silva <up201505628@fe.up.pt>
2019-11-14 19:34:43 +0100

    app :+1:

--- ideabay/resources/views/auth/register.blade.php
+++ ideabay/resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="{{{ asset('assets/favicon.ico') }}}">
-    <title>IdeaBay</title>
+    <title>Ideabay</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src={{ asset('js/app.js') }} defer></script>
--- ideabay/routes/web.php
+++ ideabay/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.homepage');
 });
 
 Route::get('/create_idea', function () {
