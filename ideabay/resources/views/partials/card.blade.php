5c96812d310a944981fc2897c408b03be53f157e
Sofia Silva <up201505628@fe.up.pt>
2019-11-20 23:44:10 +0100

    homepage frontend :white_check_mark:

---
+++ ideabay/resources/views/partials/card.blade.php
@@ -0,0 +1,15 @@
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <div class="d-flex flex-wrap keywords">
+            <a href="#" class="badge badge-secondary">Keyword</a>
+            <a href="#" class="badge badge-secondary">Keyword</a>
+            <a href="#" class="badge badge-secondary">Keyword</a>
+        </div>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <a href="#" class="btn btn-primary">Read More</a>
+    </div>
+    <div class="card-footer">
+        <small class="text-muted">Posted on 9/10/2019 by <a href="#">Author Name</a></small>
+    </div>
+</div>   
